3c03b0acfb7ccdae72b7a0a69539758861032c2c
WignerSantos <wigner12@gmail.com>
2024-01-19 14:56:27 -0300

    Adding login validations

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { Credenciais } from '../../models/credenciais';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
+
 export class LoginComponent {
 
+  creds: Credenciais = {
+    email: "",
+    senha: ""
+  }
+
+  email = new FormControl(null, Validators.email);
+  senha = new FormControl(null, Validators.minLength(3));
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  validaCampos(): boolean {
+    if(this.email.valid && this.senha.valid) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
