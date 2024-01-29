c998b2e1c9f711efa40b1a5d8b0718b1ad321929
WignerSantos <wigner12@gmail.com>
2024-01-29 12:49:41 -0300

    Adding chamado-read route

--- src/app/components/chamado/chamado-read/chamado-read.component.ts
+++ src/app/components/chamado/chamado-read/chamado-read.component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-chamado-read',
-  standalone: true,
-  imports: [],
   templateUrl: './chamado-read.component.html',
   styleUrl: './chamado-read.component.css'
 })
+
 export class ChamadoReadComponent {
 
 }
